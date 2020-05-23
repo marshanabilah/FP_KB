4d5259bb0ebc165b6fdec918281b2466cce85d25
stchffh <58497911+stchffh@users.noreply.github.com>
2020-05-23 13:11:00 +0800

    berhasil masukin algo a*

--- slidey.py
+++ slidey.py
@@ -1,8 +1,12 @@
 import pygame, sys, random
 from pygame.locals import *
+from collections import deque
+from copy import deepcopy
+import timeit
+import threading
 
-BOARDWIDTH = 4  
-BOARDHEIGHT = 4 
+BOARDWIDTH = 3  
+BOARDHEIGHT = 3 
 TILESIZE = 80
 WINDOWWIDTH = 640
 WINDOWHEIGHT = 480
@@ -34,6 +38,8 @@ DOWN = 'down'
 LEFT = 'left'
 RIGHT = 'right'
 
+
+
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init() # initialize for sound
@@ -45,7 +51,7 @@ FPSCLOCK = pygame.time.Clock() # For syncing the FPS
     
 def draw_text(surface, text, size, x, y, color):
     '''draw text to screen'''
-    font = pygame.font.Font(pygame.font.match_font('arial'), size)
+    font = pygame.font.Font(pygame.font.match_font('freesansbold.ttf'), size)
     text_surface = font.render(text, True, color)
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
@@ -62,8 +68,8 @@ def menu():
     DISPLAYSURF.blit(background, background_rect)
     DISPLAYSURF.blit(title, (0,20))
     
-    pygame.draw.rect(DISPLAYSURF, LIGHTCORAL, (130, 244, 380, 35))
-    pygame.draw.rect(DISPLAYSURF, LIGHTCORAL, (170, 295, 310, 35))
+    pygame.draw.rect(DISPLAYSURF, LIGHTCORAL, (130, 234, 380, 35))
+    pygame.draw.rect(DISPLAYSURF, LIGHTCORAL, (170, 285, 310, 35))
     draw_text(DISPLAYSURF, "PRESS [ENTER] TO BEGIN", 35, WINDOWWIDTH/2, WINDOWHEIGHT/2, WHITE)
     draw_text(DISPLAYSURF, "PRESS [Q] TO QUIT", 35, WINDOWWIDTH/2, (WINDOWHEIGHT/2) + 50, WHITE)
 
@@ -79,7 +85,171 @@ def menu():
                 sys.exit()
         elif event.type == QUIT:
             pygame.quit()
-            sys.exit()    
+            sys.exit()   
+    
+class perpetualTimer():
+
+    
+    def __init__(self,t,hFunction):
+        self.t=t
+        self.hFunction = hFunction
+        self.thread = threading.Timer(self.t,self.handle_function)
+        self.state =0
+
+    def handle_function(self):
+        self.hFunction()
+        self.thread = threading.Timer(self.t,self.handle_function)
+        self.thread.start()
+
+    def start(self):
+        self.state =1 
+        self.thread.start()
+
+    def cancel(self):
+        self.state = 0
+        self.thread.cancel()
+
+    
+def drawClock():
+    global clock_SURF, clock_RECT, timeElapsed
+    timeElapsed = timeElapsed + 1
+    clock_SURF, clock_RECT = makeText(str(timeElapsed), MESSAGECOLOR, TILECOLOR, WINDOWWIDTH - 50, WINDOWHEIGHT - 450)
+    DISPLAYSURF.blit(clock_SURF, clock_RECT)
+    pygame.display.update()
+    #print (timeElapsed)
+
+
+
+#Astar algorithm
+def astar(board):
+    startState = board
+    goalState = getStartingBoard()
+    closedStates = []
+    openStates = []
+    parent = {}
+    depth = 1
+    gcost = {}
+    fcost = {}
+
+    openStates.append([startState,""]) 
+
+    #fcost = gcost + h(n), h(n)=heuristic estimate 
+    gcost[str([startState, ""])] = 0
+    fcost[str([startState, ""])] = 0 + manhattan(startState,goalState)
+
+    #print fcost[str([startState, ""])]
+
+    while len(openStates) > 0:
+
+        smallestFCost = [sys.maxsize,-99]
+
+        for eachState in  openStates: 
+            currentFCost = fcost[str(eachState)]
+            if currentFCost < smallestFCost[0]:
+                smallestFCost= [currentFCost, eachState]
+
+        index = openStates.index(smallestFCost[1])
+        currentState = openStates.pop(index)
+
+        
+        if currentState[0] == goalState:
+            print ("found final states")
+            print ("Start State: ", startState)
+            print ("Goal State: ", goalState)
+            path = reconstructPath(startState, currentState, parent)
+            resetAstar(board, path)
+            return path
+
+        else:
+            if currentState not in closedStates:
+                closedStates.append(currentState)
+                neighborStates = getSuccessor(currentState[0])
+
+                for eachState in neighborStates:
+                    tentative_cost = gcost[str(currentState)] + getDistance(startState,currentState[0],parent) 
+
+                    if eachState not in openStates or  tentative_cost < gcost[str(eachState)]:
+                        
+                        if str(eachState[0]) not in parent.keys():
+                            parent[str(eachState[0])] = currentState
+                            gcost[str(eachState)]  = tentative_cost
+                            fcost[str(eachState)] = gcost[str(eachState)] + manhattan(eachState[0],goalState)
+                            openStates.append(eachState)
+
+#successors
+def getSuccessor(board):
+    successor = list()
+    moves = [UP, DOWN, LEFT, RIGHT]
+    
+    #If move valid
+    for eachMove in moves:
+        tempBoard = deepcopy(board)
+        if isValidMove(tempBoard, eachMove):
+            makeMove(tempBoard, eachMove)
+            successor.append([tempBoard, eachMove])
+            #print tempBoard, ",", eachMove
+    #print (successor)
+    return successor
+
+
+def reconstructPath(startState, currentState, parent):
+    
+    path = []
+    while 1:
+        path.append(currentState[1])
+        currentState = parent[str(currentState[0])]
+        #print (path)
+        if currentState[1] == '':
+            break 
+    print ("Number of Steps taken to solve: ", len(path))
+
+    return path
+
+
+def getDistance(startState, currentState, parent):
+
+    path =[]
+    if str(currentState[0]) not in parent.keys():
+        return 1
+    else: 
+        while 1:
+            path.append(currentState[1])
+            currentState = parent[str(currentState[0])]
+            #print (path)
+            if currentState[1] == '':
+                break 
+
+    return len(path)
+
+
+
+def manhattan(currentState, goalState):
+
+    totalDistance =0
+    #print currentState
+    #print goalState
+    for i in range (1,BOARDWIDTH*BOARDHEIGHT+1):
+
+  
+        for x in range(BOARDWIDTH):
+            for y in range (BOARDHEIGHT):
+                if currentState[x][y] == i:
+                    currentXY = [x,y]
+                    break 
+        
+        for x in range(BOARDWIDTH):
+            for y in range (BOARDHEIGHT):
+                if goalState[x][y] == i:
+                    goalXY = [x,y]
+                    break
+ 
+        dx = abs(currentXY[0] - goalXY[0])
+        dy = abs(currentXY[1] - goalXY[1])
+        totalDistance += (dx + dy)
+        #print totalDistance
+
+    return totalDistance
+
 
 def getStartingBoard():
     counter = 1
@@ -118,10 +288,7 @@ def makeMove(board, move):
 
 def isValidMove(board, move):
     blankx, blanky = getBlankPosition(board)
-    return (move == UP and blanky != len(board[0]) - 1) or \
-           (move == DOWN and blanky != 0) or \
-           (move == LEFT and blankx != len(board) - 1) or \
-           (move == RIGHT and blankx != 0)
+    return (move == UP and blanky != len(board[0]) - 1) or            (move == DOWN and blanky != 0) or            (move == LEFT and blankx != len(board) - 1) or            (move == RIGHT and blankx != 0)
 
 
 def getRandomMove(board, lastMove=None):
@@ -189,7 +356,11 @@ def drawBoard(board, message):
 
     DISPLAYSURF.blit(RESET_SURF, RESET_RECT)
     DISPLAYSURF.blit(NEW_SURF, NEW_RECT)
-    DISPLAYSURF.blit(SOLVE_SURF, SOLVE_RECT)
+    DISPLAYSURF.blit(ASTAR_SURF, ASTAR_RECT)
+    DISPLAYSURF.blit(timer_SURF, timer_RECT)
+    DISPLAYSURF.blit(clock_SURF, clock_RECT)
+    DISPLAYSURF.blit(move_SURF, move_RECT)
+    DISPLAYSURF.blit(numMove_SURF, numMove_RECT)
 
 
 def slideAnimation(board, direction, message, animationSpeed):
@@ -261,12 +432,26 @@ def resetAnimation(board, allMoves):
             oppositeMove = RIGHT
         slideAnimation(board, oppositeMove, '', animationSpeed=int(TILESIZE / 2))
         makeMove(board, oppositeMove) 
+        
+def resetAstar(board, allMoves):
+    # make all of the moves in allMoves in reverse.
+    revAllMoves = allMoves[:] # gets a copy of the list
+    revAllMoves.reverse()
+
+    for move in revAllMoves:
+        slideAnimation(board, move, '', animationSpeed=int(TILESIZE / 2))
+        makeMove(board, move)
                         
 
 def main(): 
     '''main loop'''
-    global FPSCLOCK, DISPLAYSURF, BASICFONT, RESET_SURF, RESET_RECT, NEW_SURF, NEW_RECT, SOLVE_SURF, SOLVE_RECT
-    
+    global FPSCLOCK, DISPLAYSURF, BASICFONT, RESET_SURF, RESET_RECT, NEW_SURF, NEW_RECT
+    global ASTAR_SURF, ASTAR_RECT
+    global timer_SURF, timer_RECT
+    global clock_SURF, clock_RECT
+    global move_SURF, move_RECT, numMove_SURF, numMove_RECT
+    global timeElapsed 
+
     background = pygame.image.load(r'C:\Users\lenovo\Pictures\wheat.jpg') 
     background = pygame.transform.scale(background, (640,480))
     background_rect = background.get_rect() 
@@ -280,12 +465,22 @@ def main():
             pygame.time.delay(1500)
             RESET_SURF, RESET_RECT = makeText('Reset',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 90)
             NEW_SURF,   NEW_RECT   = makeText('New Game', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 60)
-            SOLVE_SURF, SOLVE_RECT = makeText('Solve',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 30)
+            ASTAR_SURF, ASTAR_RECT = makeText('A* Solver',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 30)
+            timer_SURF, timer_RECT = makeText('Timer:',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 450)
+            clock_SURF, clock_RECT = makeText('0',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 50, WINDOWHEIGHT - 450)
+            move_SURF, move_RECT = makeText('Moves:',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 420)
+            numMove_SURF, numMove_RECT = makeText('0',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 50, WINDOWHEIGHT - 420)
+
 
             mainBoard, solutionSeq = generateNewPuzzle(80)
             SOLVEDBOARD = getStartingBoard() 
             allMoves = [] 
-           
+            
+            moves = 0
+            timeElapsed = 1
+            t = perpetualTimer(1, drawClock)
+            t.start()
+
         show_menu = False
         FPSCLOCK.tick(FPS) # number of FPS per loop
         pygame.display.flip()
@@ -294,6 +489,7 @@ def main():
         msg = 'Click tile or press arrow keys to slide.' 
         if mainBoard == SOLVEDBOARD:
             msg = 'Solved!'
+            t.cancel()
 
         drawBoard(mainBoard, msg)
 
@@ -304,13 +500,29 @@ def main():
 
                 if (spotx, spoty) == (None, None):
                     if RESET_RECT.collidepoint(event.pos):
-                        resetAnimation(mainBoard, allMoves) 
+                        resetAnimation(mainBoard, allMoves)
+                        clock_SURF, clock_RECT = makeText('0',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 50, WINDOWHEIGHT - 450)
+                        timeElapsed=0
+                        if t.state == 0: 
+                            t = perpetualTimer(1, drawClock)
+                            t.start()
+                        moves = 0
                         allMoves = []
                     elif NEW_RECT.collidepoint(event.pos):
-                        mainBoard, solutionSeq = generateNewPuzzle(80) 
+                        mainBoard, solutionSeq = generateNewPuzzle(80)
+                        clock_SURF, clock_RECT = makeText('0',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 50, WINDOWHEIGHT - 450)
+                        numMove_SURF, numMove_RECT = makeText('0',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 50, WINDOWHEIGHT - 420)
+                        moves =0
+                        timeElapsed =0
+                        if t.state == 0: 
+                            t = perpetualTimer(1, drawClock)
+                            t.start()
                         allMoves = []
-                    elif SOLVE_RECT.collidepoint(event.pos):
-                        resetAnimation(mainBoard, solutionSeq + allMoves) 
+                    #Astar button
+                    elif ASTAR_RECT.collidepoint(event.pos):
+                        #resetAnimation(mainBoard, solutionSeq + allMoves) # clicked on Solve button
+                        moves = len(astar(mainBoard))
+                        numMove_SURF, numMove_RECT = makeText(str(moves),    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 50, WINDOWHEIGHT - 420)
                         allMoves = []
                 else:
                     blankx, blanky = getBlankPosition(mainBoard)
@@ -326,22 +538,30 @@ def main():
             elif event.type == KEYUP:
                 if event.key in (K_LEFT, K_a) and isValidMove(mainBoard, LEFT):
                     slideTo = LEFT
+                    moves = moves+1
                 elif event.key in (K_RIGHT, K_d) and isValidMove(mainBoard, RIGHT):
                     slideTo = RIGHT
+                    moves = moves+1
                 elif event.key in (K_UP, K_w) and isValidMove(mainBoard, UP):
                     slideTo = UP
+                    moves = moves+1
                 elif event.key in (K_DOWN, K_s) and isValidMove(mainBoard, DOWN):
                     slideTo = DOWN
+                    moves = moves+1
 
         if slideTo:
             slideAnimation(mainBoard, slideTo, 'Click tile or press arrow keys to slide.', 8) 
             makeMove(mainBoard, slideTo)
             allMoves.append(slideTo) # record the slide
+
+        numMove_SURF, numMove_RECT = makeText(str(moves),    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 50, WINDOWHEIGHT - 420)
+
         pygame.display.update()
         FPSCLOCK.tick(FPS)
         
         pygame.display.flip()
         
+
         
 def terminate():
     pygame.quit()
